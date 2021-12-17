0e798c2e28b3a5d0e32e2912b0e9ce289811cfaf
Helenapati <88396481+Helenapati@users.noreply.github.com>
2021-12-17 17:01:39 -0300

    Aluna: Helena Patrícia de Souza Silva
    Turma: 2A
    Disciplina: Linguagem de Programação 1

---
+++ Main (10).java
@@ -0,0 +1,20 @@
+publkic class Questao1{
+    
+    public static void main (String arg []){
+        
+        String nome= Maria ;
+        Int numero = 6;
+        
+        try {
+            letranumber= numero.toUpperCase();
+         
+        }
+    
+        catch(NullPointerException ){
+            
+            System.out.println ("Posição inválida ");
+            
+        }
+  
+    
+    
